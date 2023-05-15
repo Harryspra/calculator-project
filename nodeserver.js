585c3bc54a6081948a89a45cd56e03916ad4faf5
Hari Prasad K <117342481+Harryspra@users.noreply.github.com>
2023-05-15 12:57:06 +0530

    Add files via upload

---
+++ nodeserver.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for the calculator page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
